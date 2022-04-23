7d9812bdbc9271f3e46c8e1a7263ee51ff292b78
Utibeabasi Umanah <utibeabasiumanah6@gmail.com>
2022-04-23 14:50:39 +0100

    updated workflow

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ const project = core.getInput('project');
 const environment = core.getInput('environment');
 
 let secrets = utils.fetchSecrets(apiKey, project, environment)
-
+core.debug(secrets)
+core.info(secrets)
 let decoded = utils.aesDecryptSecret(secrets, passCode)
 console.log(decoded);
+core.debug(decoded)
+core.info(decoded)
+
 core.setOutput("secrets", decoded)
