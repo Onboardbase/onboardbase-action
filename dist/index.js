94b41d2dbc6bfc56a3c4ee991b135ee181f2eddc
Utibeabasi Umanah <utibeabasiumanah6@gmail.com>
2022-04-23 19:23:10 +0100

    return secrets properly

--- dist/index.js
+++ dist/index.js
@@ -11685,6 +11685,10 @@ utils.fetchSecrets(apiKey, project, environment).then(secrets => {
                                 let value = decoded["value"]
                                 core.setOutput(key, value)
                             }
+                        ).catch(
+                            err => {
+                                core.setOutput(err.message)
+                            }
                         )
                     }
                 }
