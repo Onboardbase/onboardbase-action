b4a736a27d473656a2b485d55592a26cdf615056
Utibeabasi Umanah <utibeabasiumanah6@gmail.com>
2022-04-23 14:53:00 +0100

    updated workflow

--- dist/index.js
+++ dist/index.js
@@ -11670,8 +11670,12 @@ const project = core.getInput('project');
 const environment = core.getInput('environment');
 
 let secrets = utils.fetchSecrets(apiKey, project, environment)
-
+core.debug(secrets)
+core.info(secrets)
 let decoded = utils.aesDecryptSecret(secrets, passCode)
+console.log(decoded);
+core.debug(decoded)
+core.info(decoded)
 
 core.setOutput("secrets", decoded)
 })();
