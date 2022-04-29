02bb083bb8f06ca057685bae75f6e433f1c89f8b
Aleem Isiaka <aleemisiaka@gmail.com>
2022-04-29 04:46:55 +0100

    Update fix for action

--- index.ts
+++ index.ts
@@ -1,17 +1,17 @@
-import * as core from '@actions/core';
+import * as core from "@actions/core";
 import utils from "./utils";
 
-const apiKey =  core.getInput('apikey');
-const passCode =  core.getInput('passcode');
-const project =  core.getInput('project');
-const environment =  core.getInput('environment');
+const apiKey: string = core.getInput("apikey");
+const passCode: string = core.getInput("passcode");
+const project: string = core.getInput("project");
+const environment: string = core.getInput("environment");
 
 let inputs = {
-    apiKey,
-    passCode,
-    project,
-    environment
-}
+  apiKey,
+  passCode,
+  project,
+  environment,
+};
 
 // for (let item in Object.keys(inputs)){
 //     if (inputs[Object.keys(inputs)[item]] == ""){
@@ -19,36 +19,36 @@ let inputs = {
 //     }
 //     core.debug(inputs[Object.keys(inputs)[item]])
 //     console.log(inputs[Object.keys(inputs)[item]]);
-    
+
 // }
 
-utils.fetchSecrets(apiKey, project, environment).then(secrets => {
+utils
+  .fetchSecrets(apiKey, project, environment)
+  .then((secrets) => {
     for (let projectIndex in secrets["data"]["generalPublicProjects"]["list"]) {
-        let p = secrets["data"]["generalPublicProjects"]["list"][projectIndex]
-        if (p["title"] == project) {
-            for (let environIndex in p["publicEnvironments"]["list"]) {
-                let e = p["publicEnvironments"]["list"][environIndex]
-                if (e["title"] == environment) {
-                    for (const i in JSON.parse(e["key"])) {
-                        utils.aesDecryptSecret(JSON.parse(e["key"])[i], passCode).then(
-                            decoded => {
-                                decoded = JSON.parse(decoded)
-                                let key = decoded["key"]
-                                let value = decoded["value"]
-                                core.setOutput(key, value)
-                            }
-                        ).catch(
-                            err => {
-                                core.setFailed(err.message)
-                            }
-                        )
-                    }
-                }
+      let p = secrets["data"]["generalPublicProjects"]["list"][projectIndex];
+      if (p["title"] == project) {
+        for (let environIndex in p["publicEnvironments"]["list"]) {
+          let e = p["publicEnvironments"]["list"][environIndex];
+          if (e["title"] == environment) {
+            for (const i in JSON.parse(e["key"])) {
+              utils
+                .aesDecryptSecret(JSON.parse(e["key"])[i], passCode)
+                .then((decoded) => {
+                  decoded = JSON.parse(decoded);
+                  let key = decoded["key"];
+                  let value = decoded["value"];
+                  core.setOutput(key, value);
+                })
+                .catch((err) => {
+                  core.setFailed(err.message);
+                });
             }
+          }
         }
+      }
     }
-}).catch(err => {
-    core.setFailed("Unable to fetch secrets: " + err.message)})
-
-
-
+  })
+  .catch((err) => {
+    core.setFailed("Unable to fetch secrets: " + err.message);
+  });
--- utils/index.ts
+++ utils/index.ts
@@ -1,34 +1,33 @@
-import CryptoJS from 'crypto-js';
-import axios from 'axios';
-import * as core from '@actions/core';
+import CryptoJS from "crypto-js";
+import axios from "axios";
+import * as core from "@actions/core";
 
 const instance = axios.create({
-    baseURL: 'https://api.onboardbase.com/graphql',
-  });
+  baseURL: "https://api.onboardbase.com/graphql",
+});
 
-const decryptSecrets = (
-    secret,
-    passcode
-  ) => {
-    const encryptionPassphrase = passcode;
-    try {
-      const bytes = CryptoJS.AES.decrypt(secret.toString(CryptoJS.enc.Utf8), encryptionPassphrase);
-      return bytes.toString(CryptoJS.enc.Utf8);
-    } catch (error) {
-      core.setFailed("Unable to decrypt secret. Your passcode might be invalid")
-    }
-  };
+const decryptSecrets = (secret: string, passcode: string) => {
+  const encryptionPassphrase = passcode;
+  try {
+    const bytes = CryptoJS.AES.decrypt(secret.toString(), encryptionPassphrase);
+    return bytes.toString(CryptoJS.enc.Utf8);
+  } catch (error) {
+    core.setFailed("Unable to decrypt secret. Your passcode might be invalid");
+  }
+};
 
 export const aesDecryptSecret = async (secret, passcode) => {
-    return decryptSecrets(secret, passcode);
-  };
+  return decryptSecrets(secret, passcode);
+};
 
-  
-  
-  export const fetchSecrets = async (api_key, project, environment) => {
-    instance.defaults.headers['KEY'] = api_key;
-  
-    const query = `query {
+export const fetchSecrets = async (
+  api_key: string,
+  project: string,
+  environment: string
+) => {
+  instance.defaults.headers["KEY"] = api_key;
+
+  const query = `query {
       generalPublicProjects(filterOptions: { title: "${project}", disableCustomSelect: true }) {
         list {
           id
@@ -43,13 +42,15 @@ export const aesDecryptSecret = async (secret, passcode) => {
         }
       }
     }`;
-  
-    const { data } = await instance.post('', { query });
-  
-    if (data.errors && data.errors[0].message === 'Unauthorized')
-    core.setFailed("Unable to fetch secrets. You may not be authorized please check your API key")
-  
-    return data;
-  };
-  
-  export default {aesDecryptSecret, fetchSecrets}
+
+  const { data } = await instance.post("", { query });
+
+  if (data.errors && data.errors[0].message === "Unauthorized")
+    core.setFailed(
+      "Unable to fetch secrets. You may not be authorized please check your API key"
+    );
+
+  return data;
+};
+
+export default { aesDecryptSecret, fetchSecrets };
