f3995bf96c7b40b49c47daa917db28f4fd12d778
oreofeolurin <oreofeolurin@gmail.com>
2022-10-23 08:47:59 +0100

    feat(): removes node_modules and add node compilier

--- dist/utils/index.d.ts
+++
@@ -1,7 +0,0 @@
-export declare const aesDecryptSecret: (secret: any, passcode: any) => any;
-export declare const fetchSecrets: (api_key: string, project: string, environment: string) => Promise<any>;
-declare const _default: {
-    aesDecryptSecret: (secret: any, passcode: any) => any;
-    fetchSecrets: (api_key: string, project: string, environment: string) => Promise<any>;
-};
-export default _default;
